f36d812fdb2fb3368aac0a94adea5707d02459d3
Ahmed Landoulsi <ahmed.landoulsi@careem.com>
2020-06-19 14:37:05 +0400

    Fix ios

--- SharedCode/src/iosMain/kotlin/com/jarroyo/sharedcode/source/disk/SQLDriver.kt
+++ SharedCode/src/iosMain/kotlin/com/jarroyo/sharedcode/source/disk/SQLDriver.kt
@@ -2,12 +2,10 @@ package com.jarroyo.sharedcode.source.disk
 
 import com.jarroyo.sharedcode.Database
 import com.squareup.sqldelight.db.SqlDriver
-import com.squareup.sqldelight.drivers.ios.NativeSqliteDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
-actual class DbArgs(
-)
+actual class DbArgs()
 
 actual fun getSqlDriver(dbArgs: DbArgs): SqlDriver? {
-    val driver: SqlDriver = NativeSqliteDriver(Database.Schema, "test.db")
-    return driver
+    return NativeSqliteDriver(Database.Schema, "test.db")
 }
